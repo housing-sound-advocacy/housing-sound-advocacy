268a07930490308655e78416913345b6dd9d0eed
David Jensenius <david@jensenius.com>
2023-01-14 17:55:49 -0500

    Add geolocating for sound

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,49 +1,56 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { Map as MapGL } from 'mapbox-gl';
+import { Box } from '@primer/react';
 import mapboxgl from 'mapbox-gl';
-import Overlays from '../manual/overlays';
-import Circles from '../manual/circles';
-import Popups from '../popups';
+import { MapTouchEvent } from 'mapbox-gl';
 
-export default function Map() {
+export interface MapProps {
+  updateMarkers?: (lat: number, lng: number) => void;
+}
+
+export default function Map( props: MapProps) {
   mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
   const mapContainer = useRef(null);
-  const map = useRef(null);
+  const map = useRef<MapGL>(null);
   const [lng] = useState(-79.383186);
   const [lat] = useState(43.653225);
   const [zoom] = useState(13);
-  // const ward = new Ward();
-  const overlays = new Overlays();
-  const circles = new Circles();
-  const popups = new Popups();
+  const [marker] = useState(new mapboxgl.Marker());
 
   const mapStyles = {
-    width: '100vw',
-    height: '100vh',
+    width: '90vw',
+    height: '90vh',
+  };
+
+  const addMarker = (e: MapTouchEvent) => {
+    e.preventDefault();
+    const coordinates = e.lngLat;
+    props.updateMarkers(coordinates.lat, coordinates.lng);
+    marker.setLngLat(coordinates).addTo(map.current);
   };
 
   useEffect(() => {
-    console.warn('useEffect');
     if (map.current) return; // initialize map only once
-    console.warn('useEffect: initializing map');
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom,
     });
+    map.current.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+    map.current.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+    }));
+    map.current.on('click', addMarker.bind(this));
   });
-  useEffect(() => {
-    if (!map.current) return; // initialize map only once
-    map.current.on('load', () => {
-      overlays.addOverlays(map.current);
-      circles.addOverlays(map.current);
-      popups.addPopups(map.current);
-      // ward.torontoWardsJSONfromKML(map.current);
-    });
-  });
+
   return (
-    <div>
+    <Box>
       <div style={mapStyles} ref={mapContainer} className="map-container" />
-    </div>
+    </Box>
   );
 }
--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useReactMediaRecorder } from 'react-media-recorder';
 import { BaseStyles, Box, Button } from '@primer/react';
-import { CircleIcon, CheckIcon, StopIcon } from '@primer/octicons-react';
+import { CircleIcon, StopIcon } from '@primer/octicons-react';
+import Map from './Map';
 
 export default function Record() {
+
+  const reload = () => {
+    window.location.reload();
+  };
+
   const RecordView = () => {
+
+    const [hideUpload, setHideUpload] = useState(true);
+    const [markerLat, setMarkerLat] = useState(0);
+    const [markerLng, setMarkerLng] = useState(0);
+
+    const updateMarkers = (lat: number, lng: number) => {
+      setMarkerLat(lat);
+      setMarkerLng(lng);
+      setHideUpload(false);
+    };
+
+    const upload = async () => {
+      console.warn('Going to upload');
+      console.warn(`Lat: ${markerLat}`);
+      console.warn(`Lng: ${markerLng}`);
+    };
+
     const { status, startRecording, stopRecording, mediaBlobUrl } =
-      useReactMediaRecorder({ video: false, audio: true, blobPropertyBag: { type: 'audio/wav' } });
+      useReactMediaRecorder({ video: false, audio: true, blobPropertyBag: { type: 'audio/mp4' } });
 
     const recordButton = () => {
       if (status === 'idle') {
@@ -19,7 +42,9 @@ export default function Record() {
         return (
             <>
               <audio src={mediaBlobUrl} controls={true}/>
-              <Button leadingIcon={CheckIcon}>Save Recording</Button>
+              <Button onClick={reload}>Restart</Button>
+              {hideUpload ? null : <Button onClick={upload}>Upload</Button>}
+              <Map updateMarkers={updateMarkers} />
             </>
         );
       }
