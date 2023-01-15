b75169611c238ca40e17018a206d9794c5fcc020
David Jensenius <david@jensenius.com>
2023-01-15 01:43:50 -0500

    Fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Map from './pages/Map';
 import Record from './pages/Record';
 import { ThemeProvider } from '@primer/react';
@@ -9,7 +9,7 @@ export default function App() {
     <ThemeProvider>
       <Router>
         <Routes>
-          <Route path='/' element={<Record />}></Route>
+          <Route path='/record' element={<Record />}></Route>
           <Route path='/map' element={<Map />}></Route>
         </Routes>
       </Router>
