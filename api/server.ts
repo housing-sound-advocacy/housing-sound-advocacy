91ac7a680ecea39960311cc4a6dd5300d40f5f0e
David Jensenius <david@jensenius.com>
2023-01-14 20:08:08 -0500

    Connect to db and JSON sound list

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { auth } from 'express-openid-connect';
+import pg from 'pg';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -18,6 +19,15 @@ const config = {
   issuerBaseURL: process.env.AUTH_ISSUER_BASE_URL,
 };
 
+const dbConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: 'housing-sound-advocacy',
+  host: 'dfj.postgres.database.azure.com',
+  port: 5432,
+  ssl: true,
+};
+
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
@@ -26,9 +36,22 @@ app.use((req, res, next) => {
   next();
 });
 
+const client = new pg.Client(dbConfig);
+
+client.connect(err => {
+  if (err) throw err;
+});
+
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req: Request, res: Response) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/sound-list', (_req: Request, res: Response) => {
+  client.query('SELECT * FROM sounds', (err, result) => {
+    if (err) throw err;
+    res.send(result.rows);
+  });
 });
 
 app.get('/sign-up', (_req: Request, res) => {
