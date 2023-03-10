2c61d2ee4670e68d300afa201d0fdc5fa176659b
David Jensenius <david@jensenius.com>
2023-03-10 17:45:44 -0500

    Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@primer/react';
 import Map from './pages/Map';
 import Record from './pages/Record';
-import { ThemeProvider } from '@primer/react';
+import { Auth0ProviderWithNavigate } from './Auth0ProviderWithNavigate';
+import { AuthenticationGuard } from './components/AuthenticationGuard';
 
 export default function App() {
   return (
     <ThemeProvider>
       <Router>
-        <Routes>
-          <Route path='/' element={<Record />}></Route>
-          <Route path='/record' element={<Record />}></Route>
-          <Route path='/map' element={<Map />}></Route>
-        </Routes>
+        <Auth0ProviderWithNavigate>
+          <Routes>
+            <Route
+              path='/'
+              element={<AuthenticationGuard component={Record} />}
+            ></Route>
+            <Route
+              path='/record'
+              element={<AuthenticationGuard component={Record} />}
+            ></Route>
+            <Route path='/map' element={<Map />}></Route>
+          </Routes>
+        </Auth0ProviderWithNavigate>
       </Router>
     </ThemeProvider>
   );
