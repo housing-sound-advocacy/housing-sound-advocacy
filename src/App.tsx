2870e3c240b9e35e0eefbf2e7095c5b416ce5930
David Jensenius <david@jensenius.com>
2023-03-12 23:16:57 -0400

    unfuckit

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export default function App() {
   return (
     <ThemeProvider>
       <Router>
+        <Auth0ProviderWithNavigate>
           <Routes>
             <Route
               path='/'
@@ -24,6 +25,7 @@ export default function App() {
             <Route path='/map' element={<Map />} />
             <Route path='/callback' element={<Callback />} />
           </Routes>
+        </Auth0ProviderWithNavigate>
       </Router>
     </ThemeProvider>
   );
