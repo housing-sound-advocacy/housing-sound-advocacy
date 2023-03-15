83bf232d4a50d8a0f02503bd70998bb9e88d33b6
David Jensenius <david@jensenius.com>
2023-03-15 08:52:50 -0400

    Some server prep for testing

---
+++ api/__tests__/server.test.ts
@@ -0,0 +1,18 @@
+// import request from 'supertest';
+// import dotenv from 'dotenv';
+// import { Express } from 'express-serve-static-core';
+// import { createServer } from '../server';
+
+// let server: Express;
+
+beforeAll(async () => {
+  // dotenv.config({ path: '.env.test' });
+  // server = await createServer();
+});
+
+describe('GET STATIC', () => {
+  it('placeholder', () => {
+    // return request(server).get('/record').expect(200);
+    expect(true);
+  });
+});
